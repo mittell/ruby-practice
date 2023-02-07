e757cebdefe0a74fe7c1297de3e327d9bb89cdb1
Chris Mittell <mittell@hotmail.co.uk>
2023-02-07 15:52:16 +0900

    feat: added string method examples

---
+++ strings.rb
@@ -0,0 +1,17 @@
+puts "Hello I am some text!"
+print "I am another\n\"line\" of text\n"
+
+dummy = "Hey! I am some more text!"
+
+puts dummy
+puts dummy.upcase()
+puts dummy.downcase()
+puts dummy.strip()
+puts dummy.length()
+puts dummy.include? "text"
+puts dummy.include? "bacon"
+puts dummy[0]
+puts dummy[5,8]
+puts dummy.index("s")
+
+puts "test".upcase()
