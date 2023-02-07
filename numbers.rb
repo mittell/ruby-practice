9eae443a08c67e1da23d76ade97488b7d2a04845
Chris Mittell <mittell@hotmail.co.uk>
2023-02-07 16:01:26 +0900

    feat: added number examples

---
+++ numbers.rb
@@ -0,0 +1,24 @@
+puts 5
+puts 6.789
+puts -3
+puts 4 + 4.5
+puts 4 / 44
+puts 2**3
+puts 10 % 3
+
+num = 20.345
+puts ("This is a number: " + num.to_s)
+
+puts num.abs()
+puts num.round()
+puts num.ceil()
+puts num.floor()
+
+puts Math.sqrt(36)
+puts Math.log(1)
+
+puts 1 + 8
+puts 1.0 + 8
+
+puts 10 / 7
+puts 10 / 7.0
