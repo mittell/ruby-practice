86e500008f19f5b2869c21230ce490b8325676a2
Chris Mittell <mittell@hotmail.co.uk>
2023-02-12 19:16:20 +0900

    feat: added hash example

---
+++ intro/hash.rb
@@ -0,0 +1,18 @@
+consoles1 = {
+  "Sega MegaDrive" => "MD",
+  "Super Nintendo" => "SNES",
+  "Sony Playstation" => "PSX"
+}
+
+consoles2 = {
+  :MegaDrive => "MD",
+  :Playstation => "PSX"
+}
+
+
+
+puts consoles1
+puts consoles1["Sony Playstation"]
+
+puts consoles2[:MegaDrive]
+
