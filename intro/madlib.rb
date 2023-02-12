c1272df3d8cec9a838dd04b0e69d4880699c9d24
Chris Mittell <mittell@hotmail.co.uk>
2023-02-12 18:59:46 +0900

    feat: added mad lib example

---
+++ intro/madlib.rb
@@ -0,0 +1,10 @@
+puts "Enter a color:"
+color = gets.chomp()
+puts "Enter a plural noun:"
+plural = gets.chomp()
+puts "Name a person:"
+person = gets.chomp()
+
+puts ("Roses are " + color)
+puts (plural + " are blue")
+puts ("I love " + person)
