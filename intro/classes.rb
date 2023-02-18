ca277273751f4bf786ff9dc78792ce20adaf2168
Chris Mittell <mittell@hotmail.co.uk>
2023-02-18 13:41:22 +0900

    feat: updated classes example

--- intro/classes.rb
+++ intro/classes.rb
@@ -1,16 +1,21 @@
 class Book
   attr_accessor :title, :author, :pages
+  def initialize(title, author, pages)
+    @title = title
+    @author = author
+    @pages = pages
+  end
 end
 
-book1 = Book.new()
-book1.title = "Dune"
-book1.author = "Frank Herbert"
-book1.pages = 400
+book1 = Book.new("Dune", "Frank Herbert",400)
+# book1.title = "Dune"
+# book1.author = "Frank Herbert"
+# book1.pages = 400
 
-book2 = Book.new()
-book2.title = "Dune Messiah"
-book2.author = "Frank Herbert"
-book2.pages = 250
+book2 = Book.new("Dune Messiah","Frank Herbert", 250 )
+# book2.title = "Dune Messiah"
+# book2.author = "Frank Herbert"
+# book2.pages = 250
 
 puts book1.title
 puts book2.title
