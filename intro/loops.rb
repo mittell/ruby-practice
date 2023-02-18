22878760ca477c82db69efb55d015a59c79e3e1c
Chris Mittell <mittell@hotmail.co.uk>
2023-02-18 12:32:38 +0900

    feat: added for loop examples

--- intro/loops.rb
+++ intro/loops.rb
@@ -5,3 +5,20 @@ while index <= 5
   index += 1
 end
 
+friends = ["Dave", "Tommie", "Fred", "Simeon"]
+
+for friend in friends
+  puts friend
+end
+
+friends.each do |friend|
+  puts friend
+end
+
+for index in 0..5
+  puts index
+end
+
+6.times do |index|
+  puts index
+end
